d68233964676466b279f67ba953c71afa8bb694e
hong <kevin0928@naver.com>
2023-07-13 23:31:39 +0900

    init

---
+++ src/main/java/wili_be/WiliBeApplication.java
@@ -0,0 +1,13 @@
+package wili_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiliBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiliBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wili_be/WiliBeApplicationTests.java
@@ -0,0 +1,13 @@
+package wili_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiliBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
